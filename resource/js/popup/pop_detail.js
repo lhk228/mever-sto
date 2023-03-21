2fe5de7e17e996f226b926849dce7f8f912664c0
khl9880mever <khl9880@mever.me>
2023-03-21 17:38:39 +0900

    shon
    21.03 ready

--- resource/js/popup/pop_detail.js
+++ resource/js/popup/pop_detail.js
@@ -39,27 +39,25 @@ function goalRateBarControl(cnt = 12458, totalCnt = 50000)
 
 //플로우바 클릭 : 해당하는 페이지 오픈, 선택클래스 부여
 $(document).on('click','.flow-bar',function(){
-	$(".flow-bar").removeClass("selected");
-	$(this).toggleClass('selected');
-	$(`.flow-view-box`).fadeIn().removeClass("hidden");
-	$(`.flow-list-container`).hide(0);
-	console.log("hi")
+	var fileNameSplit = $('#FILE_UPLOAD').val().split('\\').pop();
+	if($('#TITLE_INPUT').val().length !==0 && $('#CONTEXT_INPUT').val().length !==0){
+		$(".flow-bar").removeClass("selected");
+		$(this).toggleClass('selected');
+		$(`#READY_PAGE`).fadeIn().removeClass("hidden");
+		$(`.flow-list-container`).hide(0);
+		$(`#TITLE`).text($('#TITLE_INPUT').val());
+		$(`#CONTEXT`).text($('#CONTEXT_INPUT').val());
+		$(`#UPLOAD_READY`).text(fileNameSplit);
+		console.log("hi ready")
+	}else{
+		$(".flow-bar").removeClass("selected");
+		$(this).toggleClass('selected');
+		$(`#INPUT_PAGE`).fadeIn().removeClass("hidden");
+		$(`.flow-list-container`).hide(0);
+		console.log("hi input")
+	}
+
 })
-
-$(document).on('click','.flow-bar',function(){
-
-	$(".flow-bar").removeClass("selected");
-	$(this).toggleClass('selected');
-	$(`.flow-view-box`).fadeIn().removeClass("hidden");
-	$(`.flow-list-container`).hide(0);
-	console.log("hi")
-})
-
-
-
-
-
-
 
 //플로우리스트 클릭 : 해당하는 페이지 오픈, 선택클래스 부여
 $(document).on('click','.flow-list',function(e){
@@ -84,20 +82,44 @@ let g_save_flow_obj;
 //click ADD LIST BUTTON
 $(document).on('click','#ADD_FLOW_LIST_BUTTON',function(){ initFlowChartWrite() });
 
-
 //click FLOW SAVE BUTTON
 $(document).on('click','#BTN_SAVE_FLOWCHART',function(){
-	flowChartClose();
+	
 	let obj = makeFlowObejct();
 	let url = "/saveFlowPost";
-
-	console.log(`makeFlowObejct :`,obj);
-	
+	if($('#TITLE_INPUT').val().length !== 0 && $('#CONTEXT_INPUT').val().length !== 0){
+	flowChartClose();
 	// g_save_flow_obj = obj;
-
 	makeFlowChartList(obj);
+	console.log(`makeFlowObejct :`,obj);
 	return;
 	api_post(url,obj);
+	}else{
+		customAlert('Please, fill the both input')
+
+	}
+	
+});
+// -- BTN_EDIT_FLOWCHART
+$(document).on('click','#BTN_EDIT_FLOWCHART',function(){
+	var titleFlow =$("#TITLE").text();
+	var contextFlow = $("#CONTEXT").text();
+	$("#EDIT_TITLE_INPUT").val(titleFlow);
+	$("#EDIT_CONTEXT_INPUT").val(contextFlow);
+	$("#TITLE, #CONTEXT").hide();
+	$("#EDIT_TITLE_INPUT, #EDIT_CONTEXT_INPUT, #BTN_NEWSAVE_FLOWCHART").show();
+	$(this).hide();
+});
+
+$(document).on('click','#BTN_NEWSAVE_FLOWCHART',function(){
+	var editedTitle =$("#EDIT_TITLE_INPUT").val();
+	var editedContext =$("#EDIT_CONTEXT_INPUT").val()
+	$("#TITLE").text(editedTitle);
+	$("#CONTEXT").text(editedContext);
+	$("#EDIT_TITLE_INPUT, #EDIT_CONTEXT_INPUT, #BTN_NEWSAVE_FLOWCHART").hide();
+	$("#TITLE, #CONTEXT").show();
+	$("#BTN_EDIT_FLOWCHART").show();
+	console.log();
 });
 
 
@@ -123,17 +145,7 @@ $(document).on('click','#BTN_FLOW_LEFT',function(){
 $(document).on('click','#BTN_WRITE_FLOW',function(){
 	console.log('작성모드')
 })
-
-
-//-- makeFlowChart input Object
-function makeFlowObejct()
-{
-	let title = $(`#TITLE_INPUT`).val();
-	let context = $(`#CONTEXT_INPUT`).val();
-	let obj = { title, context };
-
-	return obj;
-}
+ 
 
 //-- make flow list
 function makeFlowChartList(obj)
@@ -161,4 +173,15 @@ function flowChartClose(){
 	$(`.flow-view-box`).hide(0);
 	$(`.flow-list-container`).fadeIn();
 	$(".flow-bar").removeClass("selected");
+}
+
+//-- makeFlowChart input Object
+function makeFlowObejct()
+{
+	let title = $(`#TITLE_INPUT`).val();
+	let context = $(`#CONTEXT_INPUT`).val();
+	let upload = $(`#FILE_UPLOAD`).val();
+	let obj = { title, context, upload };
+
+	return obj;
 }
